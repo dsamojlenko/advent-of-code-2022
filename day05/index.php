3dcde47514ca89119eeb42de695018f90ded6d10
Dave Samojlenko <dsamojlenko@gmail.com>
2022-12-05 18:29:10 -0500

    Solved

--- day05/index.php
+++ day05/index.php
@@ -7,8 +7,6 @@ $moves = $input[1];
 
 $reversed = array_reverse(explode("\n", $stacks));
 
-print_r($reversed);
-
 $stackArray = [];
 
 foreach($reversed as $index => $crates)
@@ -16,8 +14,52 @@ foreach($reversed as $index => $crates)
     if($index < 1) continue;
 
     for ($i = 1; $i <= 33; $i+=4) {
-        $stackArray[$index -1][] = substr($crates, $i , 1);
+        $stackArray[$index][] = trim(substr($crates, $i , 1));
     }
 }
 
-print_r($stackArray);
+$columns = [];
+
+for ($i = 0; $i < 9; $i++) {
+    array_push($columns, array_filter(array_column($stackArray, $i)));
+}
+
+$moves = array_map(function($move) {
+    $exploded = explode(" ", $move);
+    return [
+        $exploded[0] => $exploded[1],
+        $exploded[2] => $exploded[3],
+        $exploded[4] => $exploded[5]
+    ];
+}, explode("\n", $moves));
+
+foreach($moves as $act)
+{
+    foreach(range(1, $act['move']) as $index) {
+        array_push($columns[$act['to']-1], array_pop($columns[$act['from']-1]));
+    }
+}
+
+foreach($columns as $column) {
+    print end($column);
+}
+
+print PHP_EOL;
+
+// Part 2
+$columns = [];
+
+for ($i = 0; $i < 9; $i++) {
+    array_push($columns, array_filter(array_column($stackArray, $i)));
+}
+
+foreach($moves as $move)
+{
+    array_push($columns[$move['to']-1], ...array_splice($columns[$move['from']-1], -$move['move'], $move['move']));
+}
+
+foreach($columns as $column) {
+    print end($column);
+}
+
+print PHP_EOL;
